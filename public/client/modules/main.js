4ba23d232c0e5554b8523b63fef9a2b975714ef9
Gregory Fabry <glfabry2@illinois.edu>
2016-01-15 15:24:21 -0600

    beginning user app selection

--- public/client/modules/main.js
+++ public/client/modules/main.js
@@ -33,7 +33,8 @@ define('main', ['exports', 'checkerboard', 'mithril', 'autoconnect', 'login', '.
   var appSelector = {
     'controller': function(args) {
       return {
-        'state': m.prop(0)
+        'state': m.prop(0),
+        'selectedApp': m.prop(null)
       };
     },
     'view': function(ctrl, args) {
@@ -41,30 +42,73 @@ define('main', ['exports', 'checkerboard', 'mithril', 'autoconnect', 'login', '.
       var classroom = store.classrooms[args.classroom];
       var apps = store.apps;
       var student = args.student;
+      var contents;
       if (ctrl.state() === 0)
-        return m('.container',
-          m('.row',
-            m('br'),
-            m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4',
-              m('.alert.alert-info', "Howdy " + classroom.users[student].name + "! Pick a room or create your own."),
-              m('.list-group',
-                _.keys(classroom.configuration.instances).map(function(instance) {
-                  return m('a.list-group-item',
-                    m('.list-group-item-heading', instance.name || apps[instance.app].name),
-                    m('.list-group-item-text', "With ...")
+        contents = [
+          m('.alert.alert-info', "Howdy ", m('strong', classroom.users[student].name), "! Pick a room or create your own. ", m('small', {'onclick': function() { location.reload() }}, m('a', "(Not you? Click here)"))),
+          m('.list-group',
+            _.keys(classroom.configuration.instances).map(function(instance) {
+              return m('a.list-group-item',
+                m('.list-group-item-heading', instance.name || apps[instance.app].name),
+                m('.list-group-item-text', "With ...")
+              );
+            }),
+            m('a.list-group-item', {
+              'onclick': function() {
+                ctrl.state(1);
+              }
+            }, m('strong', "Create a room "), m('span.hoverlight.glyphicon.glyphicon-chevron-right'))
+          )
+        ];
+      else if (ctrl.state() === 1)
+        contents = [
+          m('.panel.panel-default',
+            m('.panel-heading', m('span.glyphicon.glyphicon-chevron-left.hoverlight', {'onclick': function() { ctrl.state(0); }}), " Create a room: select an app"),
+            m('.panel-body',
+              m('#apps',
+                _.keys(store.apps).map(function(app) {
+                  return m('.app',
+                    m('img', {
+                      'src': '/apps/' + app + '/' + store.apps[app].icon,
+                      'onclick': function(e) {
+                        ctrl.selectedApp(app);
+                        ctrl.state(2);
+                      },
+                      'onmouseover': function(e) {
+                        e.target.parentNode.style['opacity'] = 1;
+                      },
+                      'onmouseout': function(e) {
+                        e.target.parentNode.style['opacity'] = 0.6;
+                      }
+                    }),
+                    m('p', store.apps[app].title)
                   );
-                }),
-                m('a.list-group-item', {
-                  'onclick': function() {
-                    ctrl.state(1);
-                  }
-                }, m('strong', "Create a room"))
+                })
               )
             )
           )
-        );
+        ];
+      else if (ctrl.state() === 2) 
+        contents = [
+          m('.panel.panel-default',
+            m('.panel-heading', m('span.glyphicon.glyphicon-chevron-left.hoverlight', {'onclick': function() { ctrl.state(1); }}), " Create a room: create a name"),
+            m('.panel-body',
+              m('#instanceName',
+                m('div',
+                  m('img', {'src': '/apps/' + ctrl.selectedApp() + '/' + store.apps[ctrl.selectedApp()].icon}),
+                  m('p', store.apps[ctrl.selectedApp()].title)
+                )
+              )
+            )
+          )
+        ];
         
-      return m('div');
+      return m('.container',
+          m('.row',
+            m('br'),
+            m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4', contents)
+          )
+        );
     }
   };
 });
--- public/shared/login.js
+++ public/shared/login.js
@@ -19,16 +19,21 @@ define('login', ['exports', 'mithril', 'underscore'], function(exports, m, _) {
       };
     },
     'view': function(ctrl, args) {
-      if (ctrl.classroom() === null) {
-        return m('.container',
-          m('.row', [
-            m('br'),
-            m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4', [
-              m('.alert.alert-info', "Log in to get started:"),
-              m('h4', 'Select a classroom'),
+      return m('.container',
+        m('.row#classroom', [
+          m('br'),
+          m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4', [
+            m('.alert.alert-info', "Log in to get started:"),
+            m('h4', 'Select a classroom'),
+            m('.list-group',
               Object.keys(args.store.classrooms).map(function(classroomId) {
                 return m('a.list-group-item', {
-                  'onclick': function() {
+                  'onclick': function(e) {
+                    var others = e.target.parentNode.children;
+                    for (var i = 0; i < others.length; i++)
+                      others[i].classList.remove('active');
+                    e.target.classList.add('active');
+                    document.getElementById('classroom').classList.add('dim');
                     if (args.student)
                       ctrl.classroom(classroomId);
                     else
@@ -36,26 +41,27 @@ define('login', ['exports', 'mithril', 'underscore'], function(exports, m, _) {
                   }
                 }, args.store.classrooms[classroomId].name)
               })
-            ])
+            )
           ])
-        );
-      } else {
-        return m('.container',
-          m('div.row', [
-            m('br'),
-            m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4', [
-              m('h4', 'What\'s your name?'),
-              Object.keys(args.store.classrooms[ctrl.classroom()].users).map(function(userId) {
+        ]),
+        m('.row', {
+          'style': ctrl.classroom() === null ? 'visibility: hidden;' : ''
+        },
+          m('br'),
+          m('.col-xs-10.col-xs-offset-1.col-sm-6-col-sm-offset-3.col-md-4.col-md-offset-4', [
+            m('h4', 'What\'s your name?'),
+            m('.list-group',
+              Object.keys(ctrl.classroom() ? args.store.classrooms[ctrl.classroom()].users : {}).map(function(userId) {
                 return m('a.list-group-item', {
                   'onclick': function() {
                     args.callback(ctrl.classroom(), userId);
                   }
                 }, args.store.classrooms[ctrl.classroom()].users[userId].name)
               })
-            ])
+            )
           ])
-        );
-      } 
+        )
+      );
     }
   }
 });
