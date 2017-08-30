3e9ccdf657f4169c40295fa450579089a1d774b4
Ian Wells <iwells018@gmail.com>
2017-08-30 09:32:17 -0500

    added two more colors

--- public/shared/userColors.js
+++ public/shared/userColors.js
@@ -1,7 +1,7 @@
 define(["exports"], function(exports) {
 
   //var userColors = ["#FF0000", "#00FF00", "#0000FF", "#F0F000", "#00F0F0"];
-  var userColors = ["#ac63a5", "#face57", "#e98039", "#6ab1b6"];
+  var userColors = ["#ac63a5", "#face57", "#e98039", "#6ab1b6", "#7f7f7f", "#cccccc"];
 
   exports.userColors = userColors;
   exports.getColor = function(userList, id) {
