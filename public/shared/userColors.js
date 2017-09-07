e58408dd563e3ee854883c7ca50a00dfdefe6100
Ian Wells <iwells018@gmail.com>
2017-09-07 16:47:30 -0500

    add more colors

--- public/shared/userColors.js
+++ public/shared/userColors.js
@@ -1,7 +1,20 @@
 define(["exports"], function(exports) {
 
   //var userColors = ["#FF0000", "#00FF00", "#0000FF", "#F0F000", "#00F0F0"];
-  var userColors = ["#ac63a5", "#face57", "#e98039", "#6ab1b6", "#7f7f7f", "#cccccc"];
+  var userColors = [
+      "#ac63a5", // purple
+      "#face57", // yellow
+      "#e98039", // orange
+      "#6ab1b6", // light blue
+      "#ff0000", // full red
+      "#00ff00", // full green
+      "#ffff00", // full yellow
+      "#0000ff", // full blue
+      "#ff00ff", // magenta
+      "#00ffff", // teal
+      "#7f7f7f", // gray
+      "#cccccc" // darker gray
+  ];
 
   exports.userColors = userColors;
   exports.getColor = function(userList, id) {
